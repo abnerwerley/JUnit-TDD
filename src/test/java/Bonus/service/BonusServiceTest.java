396b84f6a7612e962b6c84c421fdc51eb563b918
Abner Werley <abnerwerley77@gmail.com>
2022-08-22 18:56:52 -0300

    @abnerwerley[Testes para o BonusService]

---
+++ src/test/java/Bonus/service/BonusServiceTest.java
@@ -0,0 +1,41 @@
+package Bonus.service;
+
+
+import br.com.alura.tdd.modelo.Funcionario;
+import br.com.alura.tdd.service.BonusService;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class BonusServiceTest {
+
+    @Test
+    void bonusDeveriaSer0ParaFuncionarioComSalarioMuitoAlto() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Gabriel", LocalDate.now(), new BigDecimal("25000")));
+
+        assertEquals(new BigDecimal("0.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoDoSalario() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Gabriel", LocalDate.now(), new BigDecimal("2500")));
+
+        assertEquals(new BigDecimal("250.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveSer10PorCentoParaSalarioDe10Mil() {
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Gabriel", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.00"), bonus);
+    }
+
+
+}
